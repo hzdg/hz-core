d99b63b32ac250976882de618765dac147fd4070
elainen <elaineh@hzdg.com>
2018-03-22 16:39:47 -0400

    Fix bash function name

--- scripts/update-docker-tags.sh
+++ scripts/update-docker-tags.sh
@@ -5,10 +5,10 @@ set -e
 my_dir="$(dirname "$0")"
 source "$my_dir/pre-check.sh"
 
-update-docker-tags () {
+update_docker_tags () {
   docker build -t $DOCKER_RESPOSITRY .
   docker tag $DOCKER_RESPOSITRY $DOCKER_RESPOSITRY:$CURRENT_VERSION_NUMBER
   docker push $DOCKER_RESPOSITRY
 }
 
-update-docker-tags
+update_docker_tags
