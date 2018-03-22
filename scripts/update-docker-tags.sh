90b2472595d848b9a7f818a9c17119463cef3e14
elainen <elaineh@hzdg.com>
2018-03-22 16:42:14 -0400

    Fix missssspelling

--- scripts/update-docker-tags.sh
+++ scripts/update-docker-tags.sh
@@ -6,9 +6,9 @@ my_dir="$(dirname "$0")"
 source "$my_dir/pre-check.sh"
 
 update_docker_tags () {
-  docker build -t $DOCKER_RESPOSITRY .
-  docker tag $DOCKER_RESPOSITRY $DOCKER_RESPOSITRY:$CURRENT_VERSION_NUMBER
-  docker push $DOCKER_RESPOSITRY
+  docker build -t $DOCKER_REPOSITORY .
+  docker tag $DOCKER_REPOSITORY $DOCKER_REPOSITORY:$CURRENT_VERSION_NUMBER
+  docker push $DOCKER_REPOSITORY
 }
 
 update_docker_tags
