f6d7903c39ed248239889bdef8f468b296ed1667
elainen <elaineh@hzdg.com>
2018-04-04 13:15:14 -0400

    Add custom styles for styleguidist

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,14 +1,30 @@
 const {createConfig, babel, css} = require('webpack-blocks');
+const path = require('path');
 
 module.exports = {
   components: 'packages/**/src/**.js',
   webpackConfig: createConfig([babel(), css()]),
   showUsage: true,
+  template: path.join(__dirname, 'conf', 'styleguide', 'template.html'),
+  require: [path.join(__dirname, 'conf', 'styleguide', 'styles.css')],
   theme: {
     color: {
       link: '#f38230',
       linkHover: 'rgb(67, 69, 77)',
       border: '#BABABA',
+    },
+  },
+  styles: {
+    ReactComponent: {
+      root: {
+        padding: '35px',
+        boxShadow: '0 0 10px 1px #e8e8e8',
+      },
+    },
+    SectionHeading: {
+      heading: {
+        marginTop: 50,
+      },
     },
   },
   getExampleFilename(componentPath) {
