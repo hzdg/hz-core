c4e82cad9039d29a8d23cc6b55b7b41fc3c2470e
elainen <elaineh@hzdg.com>
2018-04-05 13:34:38 -0400

    Fix custom template with newer styleguidist config

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,12 +1,28 @@
 const {createConfig, babel, css} = require('webpack-blocks');
+const {generateJSReferences} = require('mini-html-webpack-plugin');
 const path = require('path');
+const fs = require('fs');
+
+function resolve(filePath, {js, publicPath}) {
+  const jsReference = generateJSReferences(js, publicPath);
+  return fs
+    .readFileSync(filePath)
+    .toString()
+    .replace('<!-- jsReference -->', jsReference);
+}
 
 module.exports = {
   components: 'packages/**/src/**.js',
   webpackConfig: createConfig([babel(), css()]),
   showUsage: true,
   pagePerSection: true,
+  title: 'HZ Core Component Library',
   require: [path.join(__dirname, 'conf', 'styleguide', 'styles.css')],
+  template: ({js, publicPath}) =>
+    resolve(path.join(__dirname, 'conf', 'styleguide', 'template.html'), {
+      js,
+      publicPath,
+    }),
   theme: {
     color: {
       link: '#f38230',
