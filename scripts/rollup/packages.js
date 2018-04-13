14d7b73604fbc4f123db60c045536be2b60eb663
Eric Eldredge <lettertwo@gmail.com>
2018-04-13 18:05:04 -0400

    Update rollup packages script to use workspace packages

--- scripts/rollup/packages.js
+++ scripts/rollup/packages.js
@@ -1,31 +1,35 @@
-const {readdirSync} = require('fs');
 const path = require('path');
+const glob = require('glob');
 
 const PROJECT_ROOT = path.resolve(__dirname, '..', '..');
 const PROJECT_DIST_DIRECTORY = path.resolve(PROJECT_ROOT, 'build', 'dist');
-const PACKAGE_DIRECTORY = path.resolve(PROJECT_ROOT, 'packages');
 
-// Append any information needed for each package
-const gatherPackages = () => {
-  const packageList = readdirSync(PACKAGE_DIRECTORY);
-  const packageListObject = packageList.map(packageName => {
-    const packageDirectory = path.join(PACKAGE_DIRECTORY, packageName);
+const {workspaces} = require(path.join(PROJECT_ROOT, 'package.json'));
+
+const packages = workspaces
+  // Find all of the package.json files in package workspaces.
+  .reduce(
+    (matches, pattern) =>
+      matches.concat(
+        glob.sync(path.join(PROJECT_ROOT, pattern, 'package.json')),
+      ),
+    [],
+  )
+  // Append any information needed for each package.
+  .map(pkg => {
+    const packageDirectory = path.dirname(pkg);
+    const packageName = path.basename(packageDirectory);
     const packageDistDirectory = path.join(PROJECT_DIST_DIRECTORY, packageName);
     return {
       packageName,
       packageDirectory,
       packageDistDirectory,
-      bundleInput: path.join(PACKAGE_DIRECTORY, packageName, 'src', 'index.js'),
+      bundleInput: pkg,
       bundleOutput: {
-        file: path.join(PROJECT_DIST_DIRECTORY, packageName, 'index.js'),
+        file: path.join(packageDistDirectory, 'index.js'),
         format: 'cjs',
       },
     };
   });
 
-  return packageListObject;
-};
-
-module.exports = {
-  packages: gatherPackages(),
-};
+module.exports = {packages};
