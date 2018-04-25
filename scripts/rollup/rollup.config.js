c673a33e4fde85bbc057e2dbd7aca0f002a8884c
elainen <elaineh@hzdg.com>
2018-04-25 11:34:06 -0400

    Fix build error

--- scripts/rollup/rollup.config.js
+++ scripts/rollup/rollup.config.js
@@ -1,7 +1,9 @@
 const babel = require('rollup-plugin-babel');
+const json = require('rollup-plugin-json');
 
 module.exports = {
   plugins: [
+    json(),
     babel({
       exclude: 'node_modules/**',
     }),
