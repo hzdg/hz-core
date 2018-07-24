8ccb2d7082ba9f999a49f5838e723918a49832ac
Eric Eldredge <lettertwo@gmail.com>
2018-07-24 11:05:07 -0400

    Fix ScrollObservable unsubscription

--- packages/hzcore-scroll-monitor/src/ScrollObservable.js
+++ packages/hzcore-scroll-monitor/src/ScrollObservable.js
@@ -4,44 +4,55 @@ import {Observable, getScrollRect} from './utils';
 
 import type {ObserverSet} from './types';
 
+const SCROLL = 'scroll';
+const LISTENER_OPTIONS = {passive: true};
+
 const debug = Debug('ScrollMonitor:scroll');
 
-const elements: Map<HTMLElement | Document, ObserverSet> = new Map();
+const elements: Map<
+  HTMLElement | Document,
+  {observers: ObserverSet, handler(event: Event): void},
+> = new Map();
 
-export function create(element: HTMLElement | Document): Observable {
+function createScrollHandler(element) {
   const handleScroll = (scrollEvent: Event) => {
-    const observers = elements.get(element);
     const target = scrollEvent.currentTarget;
+    const elementConfig = elements.get(element);
     if (
-      (target instanceof HTMLElement || target instanceof Document) &&
-      observers
+      elementConfig &&
+      (target instanceof HTMLElement || target instanceof Document)
     ) {
-      observers.forEach(observer => {
+      elementConfig.observers.forEach(observer => {
         observer.next({rect: getScrollRect(target)});
       });
     }
   };
+  return handleScroll;
+}
 
+export function create(element: HTMLElement | Document): Observable {
   return new Observable(observer => {
-    const observers: ObserverSet = elements.get(element) || new Set();
-    if (!elements.has(element)) {
+    let elementConfig = elements.get(element);
+    if (!elementConfig) {
       debug('Creating scroll event listener', element);
-      element.addEventListener('scroll', handleScroll, {passive: true});
-      elements.set(element, observers);
+      const observers = new Set();
+      const handler = createScrollHandler(element);
+      elementConfig = {observers, handler};
+      element.addEventListener(SCROLL, handler, LISTENER_OPTIONS);
+      elements.set(element, elementConfig);
     }
-
     debug('Subscribing to scroll events', element, observer);
-    observers.add(observer);
+    elementConfig.observers.add(observer);
 
     return {
       unsubscribe() {
-        debug('Unsubscribing from scroll events', element, observer);
-        observers.delete(observer);
-        if (!observers.size) {
-          elements.delete(element);
-          if (!elements.get(element)) {
+        if (elementConfig) {
+          debug('Unsubscribing from scroll events', element, observer);
+          const {observers, handler} = elementConfig;
+          observers.delete(observer);
+          if (!observers.size) {
             debug('No observers left! Cleaning up scroll listener...', element);
-            element.removeEventListener('scroll', handleScroll);
+            element.removeEventListener(SCROLL, handler, LISTENER_OPTIONS);
             elements.delete(element);
           }
         }
