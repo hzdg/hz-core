c2631b7fb7b142b2e371e974e1fa8983fe7a8e67
Andrey Morozov <andreym@hzdg.com>
2019-03-15 16:49:10 -0400

    (@hzcore/use-hover) removing focus events
    :(

--- packages/behaviors/hzcore-use-hover/src/index.js
+++ packages/behaviors/hzcore-use-hover/src/index.js
@@ -7,36 +7,25 @@ import type {ReturnedHoverProps, Props} from './types.js';
 export default function useHover({
   mouseEnterDelayMS = 0,
   mouseLeaveDelayMS = 0,
-  focusable = false,
 }: Props): [boolean, ReturnedHoverProps] {
   const [isHovering, setIsHovering] = useState(false);
   let mouseEnterTimer;
   let mouseOutTimer;
 
-  function HoverPropsFactory(): ReturnedHoverProps {
-    const onMouseEnter = () => {
+  const hoverProps = {
+    onMouseEnter: () => {
       clearTimeout(mouseOutTimer);
       mouseEnterTimer = setTimeout(
         () => setIsHovering(true),
         mouseEnterDelayMS,
       );
-    };
+    },
 
-    const onMouseLeave = () => {
+    onMouseLeave: () => {
       clearTimeout(mouseEnterTimer);
       mouseOutTimer = setTimeout(() => setIsHovering(false), mouseLeaveDelayMS);
-    };
+    },
+  };
 
-    const onBlur = focusable ? onMouseLeave : null;
-    const onFocus = focusable ? onMouseEnter : null;
-
-    return {
-      onMouseEnter,
-      onMouseLeave,
-      onBlur,
-      onFocus,
-    };
-  }
-
-  return [isHovering, HoverPropsFactory()];
+  return [isHovering, hoverProps];
 }
--- packages/behaviors/hzcore-use-hover/src/types.js
+++ packages/behaviors/hzcore-use-hover/src/types.js
@@ -1,12 +1,9 @@
 export type ReturnedHoverProps = {
   onMouseEnter: () => void,
   onMouseLeave: () => void,
-  onBlur: null | (() => void),
-  onFocus: null | (() => void),
 };
 
 export type Props = {
   mouseEnterDelayMS?: number,
   mouseLeaveDelayMS?: number,
-  focusable?: boolean,
 };
