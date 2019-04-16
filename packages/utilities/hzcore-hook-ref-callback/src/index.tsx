ba404a78d4004c82ebc500ecec8d69e34e2e72b2
Eric Eldredge <lettertwo@gmail.com>
2019-04-16 19:14:41 -0400

    📝 (@hzcore/hook-ref-callback) document use cases

--- packages/utilities/hzcore-hook-ref-callback/src/index.tsx
+++ packages/utilities/hzcore-hook-ref-callback/src/index.tsx
@@ -1,6 +1,18 @@
 import React, {useCallback, useState, useRef} from 'react';
 
+/**
+ * A hook for components that need to know when a ref changes.
+ *
+ * Returns a tuple like `[ref, setRef]`, where `ref` is a ref object,
+ * and `setRef` is a ref callback. Whenever the ref callback is called,
+ * the ref object is updated with the value passed to the callback, and the
+ * component is rerendered.
+ */
 export default function useRefCallback<T>(
+  /**
+   * An optional ref object or callback ref.
+   * Useful when the component needs to handle ref forwarding.
+   */
   innerRef?: ((node: T | null) => void) | React.MutableRefObject<T | null>,
 ): [(node: T | null) => void, React.RefObject<T | null>] {
   const [currentNode, setNode] = useState<T | null>(null);
