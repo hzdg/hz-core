5fa743a96dce3ec35878b12c6c9feafc1e918450
Eric Eldredge <lettertwo@gmail.com>
2019-04-25 17:34:39 -0400

    🐛 (@hzcore/scroll-monitor) fix useScrolling timeout management

--- packages/behaviors/hzcore-scroll-monitor/src/useScrolling.ts
+++ packages/behaviors/hzcore-scroll-monitor/src/useScrolling.ts
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef, useCallback, useMemo} from 'react';
 import {getNearestScrollNode} from './utils';
 import useRefCallback, {InnerRef} from '@hzcore/hook-ref-callback';
 
@@ -19,43 +19,62 @@ export default function useScrolling(
    */
   innerRef?: InnerRef<HTMLElement> | null,
 ): [boolean, (node: HTMLElement | null) => void] {
+  // Keep track of whether or not the nearest scrollable container is scrolling.
   let [scrolling, setScrolling] = useState(false);
-  let [ref, refCallback] = useRefCallback(innerRef);
-  const scrollingElement = getNearestScrollNode(ref.current);
 
+  // Keep a ref to the nearest scrollable container.
+  let [{current}, refCallback] = useRefCallback(innerRef);
+  const scrollingElement = useMemo(() => getNearestScrollNode(current), [
+    current,
+  ]);
+
+  // Keep a ref to a timeout id.
+  const scrollTimeout = useRef<NodeJS.Timeout | null>(null);
+
+  // A callback to clear the scroll timeout.
+  const clearScrollTimeout = useCallback((): void => {
+    if (scrollTimeout.current) {
+      clearTimeout(scrollTimeout.current);
+      scrollTimeout.current = null;
+    }
+  }, []);
+
+  // A callback to clear the scroll timeout and flip `scrolling` to false.
+  const stopScrolling = useCallback((): void => {
+    clearScrollTimeout();
+    setScrolling(false);
+  }, [clearScrollTimeout]);
+
+  // A callback to start the scroll timeout and flip `scrolling` to true.
+  const startScrolling = useCallback((): void => {
+    clearScrollTimeout();
+    setScrolling(true);
+    scrollTimeout.current = setTimeout(stopScrolling, SCROLL_TIMEOUT);
+  }, [clearScrollTimeout, stopScrolling]);
+
+  // Make sure we clear the timeout when we unmount.
+  useEffect(() => clearScrollTimeout(), [clearScrollTimeout]);
+
+  // Subscribe to scroll events on the nearest scrolling element,
+  // calling the `startScrolling` callback whenever a scroll event occurs.
   useEffect(() => {
-    let scrollTimeoutPending: NodeJS.Timeout | false | null;
-
-    const handleScrollTimeout = (): void => {
-      if (scrollTimeoutPending) {
-        clearTimeout(scrollTimeoutPending);
-        scrollTimeoutPending = false;
-      }
-      setScrolling(false);
-    };
-
-    const handler = (): void => {
-      if (scrollTimeoutPending) {
-        clearTimeout(scrollTimeoutPending);
-        scrollTimeoutPending = false;
-      }
-      setScrolling(true);
-      scrollTimeoutPending = setTimeout(handleScrollTimeout, SCROLL_TIMEOUT);
-    };
-
     if (scrollingElement) {
-      scrollingElement.addEventListener(SCROLL, handler, LISTENER_OPTIONS);
+      scrollingElement.addEventListener(
+        SCROLL,
+        startScrolling,
+        LISTENER_OPTIONS,
+      );
     }
     return () => {
       if (scrollingElement) {
-        scrollingElement.removeEventListener(SCROLL, handler, LISTENER_OPTIONS);
-      }
-      if (scrollTimeoutPending) {
-        clearTimeout(scrollTimeoutPending);
-        scrollTimeoutPending = false;
+        scrollingElement.removeEventListener(
+          SCROLL,
+          startScrolling,
+          LISTENER_OPTIONS,
+        );
       }
     };
-  }, [scrollingElement]);
+  }, [scrollingElement, startScrolling]);
 
   return [scrolling, refCallback];
 }
