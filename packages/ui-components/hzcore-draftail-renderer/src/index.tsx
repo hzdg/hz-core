33fbe1a13697ff0d6bdcde255295de06ffa832a8
Andrey Morozov <andreym@hzdg.com>
2019-10-16 12:59:20 -0400

    ✨ (@hzcore/draftail-renderer) making draftail parse deeply nested lists

--- packages/ui-components/hzcore-draftail-renderer/src/index.tsx
+++ packages/ui-components/hzcore-draftail-renderer/src/index.tsx
@@ -56,6 +56,47 @@ export interface DraftailRendererProps {
   components?: Components;
 }
 
+class Stack {
+  count: number = 0;
+  storage: {
+    [key: number]: Block | BlockWithEntityMap | (Block | BlockWithEntityMap)[];
+  } = {};
+  /**
+   * Append a value to the end of the stack.
+   */
+  push(
+    value: Block | BlockWithEntityMap | (Block | BlockWithEntityMap)[],
+  ): void {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+  /**
+   * Removes and returns the value at the end of the stack.
+   */
+  pop(): unknown {
+    if (this.count === 0) {
+      return undefined;
+    }
+    this.count--;
+    const result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+  }
+
+  /**
+   *  Returns the current site of the stack.
+   */
+  size(): number {
+    return this.count;
+  }
+  /**
+   * Returns the value at the end of the stack
+   */
+  peek(): unknown {
+    return this.storage[this.count - 1];
+  }
+}
+
 function groupBlocksByType(
   blocksWithEntities: (Block | BlockWithEntityMap)[],
 ): (Block | BlockWithEntityMap)[] | null {
@@ -92,108 +133,123 @@ function groupBlocksByType(
   );
 }
 
-function sortNestedLists(blocks: Block[]): Block[] {
-  let list: unknown[] | Block[] = [];
-
+function sortNestedLists(
+  blocks: (Block | BlockWithEntityMap)[],
+): (Block | BlockWithEntityMap)[] | null {
+  const stack = new Stack();
+  let currentDepth = 0;
+  if (!blocks || blocks.length === 0) return null;
   blocks.forEach(block => {
-    if (list.length === 0) {
-      list.push(block);
-    } else {
-      /**
-       * grabbing value of the last item in the list.
-       */
-      let last = list[list.length - 1]
-        ? list[list.length - 1]
-        : list[list.length];
-      /**
-       * we've moved from the first index.
-       */
-      if (Array.isArray(last)) {
-        let lastInLast = last[last.length - 1]
-          ? last[last.length - 1]
-          : last[last.length];
-
-        /**
-         * if the first item in nested array has the same depth as the current block - add the block to that array.
-         */
-        if (block.depth === last[0].depth) {
-          last.push(block);
+    if (block.depth === currentDepth) {
+      stack.push(block);
+    } else if (block.depth < currentDepth) {
+      currentDepth--;
+      let stackPeek = stack.pop();
+      if (stackPeek && Array.isArray(stackPeek)) {
+        let currentPeek = stack.peek();
+        if (Array.isArray(currentPeek)) {
+          currentPeek.push(stackPeek);
+          currentPeek.push(block);
         } else {
-          /**
-           * if not - create a new array and add the block to that array and append it to the position next to the last item, depending on the depth
-           */
-          let nestedArray: unknown[] | Block[] = [];
-          nestedArray.push(block);
-
-          if (Array.isArray(lastInLast)) {
-            if (block.depth === lastInLast[0].depth) {
-              lastInLast.push(nestedArray);
-            } else {
-              if (Array.isArray(lastInLast[lastInLast.length - 1])) {
-                if (lastInLast[lastInLast.length - 1].depth === block.depth) {
-                  lastInLast[lastInLast.length - 1].push(block);
-                } else {
-                  lastInLast[lastInLast.length - 1].push(nestedArray);
-                }
-              } else {
-                lastInLast.push(nestedArray);
-              }
-            }
-          } else {
-            last.push(nestedArray);
-          }
-        }
-        /**
-         * if the last item is array, we are one level deeper.
-         */
-      } else {
-        /**
-         * last item in the list is NOT array but a regular block.
-         */
-        if ((last as Block) && (last as Block).depth !== block.depth) {
-          let nestedArray: unknown[] | Block[] = [];
-          nestedArray.push(block);
-          list.push(nestedArray);
-        } else {
-          list.push(block);
+          stack.push(stackPeek);
+          stack.push(block);
         }
       }
+    } else if (block.depth > currentDepth) {
+      currentDepth++;
+      stack.push([block]);
+    } else {
+      // noop ? we should never get here.
     }
   });
 
-  return list as Block[];
+  // TODO: type this somehow
+  return Object.values(stack.storage);
 }
 
-function renderListRecursive(
+function renderLists(
   block: (Block | BlockWithEntityMap)[] | (Block | BlockWithEntityMap),
 ): JSX.Element | null {
   if (!Array.isArray(block)) {
     return createElementBasedOnBlockType(block);
   } else {
     return isOrderedListItem(block[0]) ? (
-      <ol key={block[0].key}>{block.map(renderListRecursive)}</ol>
+      <ol key={`ordered_${block[0].key}`}>{block.map(renderLists)}</ol>
     ) : isUnorderedListItem(block[0]) ? (
-      <ul key={block[0].key}>{block.map(renderListRecursive)}</ul>
+      <ul key={`unordered_${block[0].key}`}>{block.map(renderLists)}</ul>
     ) : null;
   }
 }
 
-function renderList(
-  block: BlockWithEntityMap,
-): JSX.Element | (JSX.Element | null)[] | null {
+function renderList(block: (Block | BlockWithEntityMap)[]): JSX.Element | null {
   const sortedNestedLists = sortNestedLists(block);
-  return renderListRecursive(sortedNestedLists);
+  if (sortedNestedLists) {
+    return renderLists(sortedNestedLists);
+  } else {
+    return null;
+  }
+}
+
+function mapEntityToBlock(
+  blocks: Block[],
+  entityMap: EntityMap,
+): (Block | BlockWithEntityMap)[] {
+  return (
+    blocks
+      .map(block => {
+        /**
+         * if the block has an entityMap assosiated with it by `key`
+         * we're mapping the data of that entityMap to the block and
+         * merging those 2 objects together.
+         */
+        if (block.entityRanges && block.entityRanges.length > 0 && entityMap) {
+          for (const key of Object.keys(entityMap as EntityMap)) {
+            if (Number(block.entityRanges[0].key) === Number(key)) {
+              let blockWithEntity;
+              /**
+               * if the block(typeof UNSTYLED) doesn't have any styles applied to it,
+               * we can merge it with its entityMap. the type will become LINK.
+               */
+              if (block.type === UNSTYLED) {
+                blockWithEntity = Object.assign({}, block, entityMap[key]);
+                return blockWithEntity;
+              } else {
+                /**
+                 * if there is a specific(not unstyled) type applied to a block,
+                 * we need to preserve that type and add a flag to an object
+                 * saying that it also should be a link. Solves an issue when a link
+                 * is a part of un/ordered list.
+                 */
+                blockWithEntity = Object.assign({}, block, entityMap[key], {
+                  type: block.type,
+                  isLink: true,
+                });
+                return blockWithEntity;
+              }
+            }
+          }
+        }
+        return block as Block;
+      })
+      /**
+       * Filtering:
+       * 1. Empty objects.
+       * 2. Blocks without type. We're doing further filtering primarily based
+       * on a type.
+       */
+      .filter(block => Boolean(Object.keys(block).length > 0 && block.type))
+  );
 }
 
 function renderBlocksWithEntities(
   blocks: (Block | BlockWithEntityMap)[],
-): JSX.Element | (JSX.Element | null)[] | null {
+): JSX.Element | JSX.Element[] | (JSX.Element | null)[] | null {
   const groupedBlocks = groupBlocksByType(blocks);
   if (!groupedBlocks || groupedBlocks.length === 0) return null;
   return groupedBlocks
     .map(block => {
       if (Array.isArray(block)) {
-        return renderList(block);
+        return renderList(block as (Block | BlockWithEntityMap)[]);
       } else {
         return createElementBasedOnBlockType(block);
       }
@@ -205,51 +261,16 @@ export default function DraftailRenderer({
   components = {},
   body = {},
 }: DraftailRendererProps): JSX.Element | null {
-  const {blocks = [], entityMap = {}} = body as RichTextNode;
+  if (
+    !body ||
+    !(body as RichTextNode).blocks ||
+    (body as RichTextNode).blocks.length === 0
+  ) {
+    return null;
+  }
 
-  const blocksWithEntities: (Block | BlockWithEntityMap)[] = blocks
-    .map(block => {
-      /**
-       * if the block has an entityMap assosiated with it by `key`
-       * we're mapping the data of that entityMap to the block and
-       * merging those 2 objects together.
-       */
-      if (block.entityRanges && block.entityRanges.length > 0 && entityMap) {
-        for (const key of Object.keys(entityMap as EntityMap)) {
-          if (Number(block.entityRanges[0].key) === Number(key)) {
-            let blockWithEntity;
-            /**
-             * if the block(typeof UNSTYLED) doesn't have any styles applied to it,
-             * we can merge it with its entityMap. the type will become LINK.
-             */
-            if (block.type === UNSTYLED) {
-              blockWithEntity = Object.assign({}, block, entityMap[key]);
-              return blockWithEntity;
-            } else {
-              /**
-               * if there is a specific(not unstyled) type applied to a block,
-               * we need to preserve that type and add a flag to an object
-               * saying that it also should be a link. Solves an issue when a link
-               * is a part of un/ordered list.
-               */
-              blockWithEntity = Object.assign({}, block, entityMap[key], {
-                type: block.type,
-                isLink: true,
-              });
-              return blockWithEntity;
-            }
-          }
-        }
-      }
-      return block as Block;
-    })
-    /**
-     * Filtering:
-     * 1. Empty objects.
-     * 2. Blocks without type. We're doing further filtering primarily based
-     * on a type.
-     */
-    .filter(block => Boolean(Object.keys(block).length > 0 && block.type));
+  const {blocks = [], entityMap = {}} = body as RichTextNode;
+  const blocksWithEntities = mapEntityToBlock(blocks, entityMap);
 
   return (
     <DraftailProvider components={components}>
