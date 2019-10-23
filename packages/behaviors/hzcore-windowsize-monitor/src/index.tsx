1c987991249a4a388abdde1180b04fea76989b7c
Eric Eldredge <lettertwo@gmail.com>
2019-10-23 18:15:51 -0400

    💥 (@hzcore/windowsize-monitor) add stateless hook API
    Allows consuming components to optimize or handle updates in
    their own way.

--- packages/behaviors/hzcore-windowsize-monitor/src/index.tsx
+++ packages/behaviors/hzcore-windowsize-monitor/src/index.tsx
@@ -1,62 +1,156 @@
-import React, {PureComponent, useState, useEffect} from 'react';
-
+import React, {
+  PureComponent,
+  useState,
+  useRef,
+  useEffect,
+  useCallback,
+  useMemo,
+} from 'react';
 import {throttle} from './utils';
 
 import {Props, State} from './types';
 
 export * from './types';
 
-const events = new Set();
-const onResize = (): void => events.forEach(fn => fn());
+export interface WindowSize {
+  width: number;
+  height: number;
+}
+
+export interface WindowSizeOptions {
+  /**
+   * How frequently window size updates are dispatched.
+   * Default is `0`, meaning, every update is dispatched.
+   */
+  throttleMs?: number;
+  /**
+   * The initial window width. Used before a measurement has been taken.
+   * Default is `0`.
+   */
+  initialWidth?: number;
+  /**
+   * The initial window height. Used before a measurement has been taken.
+   * Default is `0`.
+   */
+  initialHeight?: number;
+}
+
+export interface WindowSizeMonitorProps {
+  children: (windowSize: WindowSize) => JSX.Element;
+}
+
 const isClient = typeof window !== undefined && typeof window !== 'undefined';
 
-const subscriber = {
-  subscribe: (handler: () => void) => {
-    if (events.size === 0) {
-      window.addEventListener('resize', onResize);
+const getWindowSize = ({
+  initialWidth,
+  initialHeight,
+}: Required<WindowSizeOptions>): WindowSize => {
+  if (isClient) return {width: window.innerWidth, height: window.innerHeight};
+  return {width: initialWidth, height: initialHeight};
+};
+
+const windowSizeSubscribers = new Set();
+
+const dispatchWindowSizeChange = (): void => {
+  windowSizeSubscribers.forEach(cb => cb());
+};
+
+const subscribeToWindowSizeChange = (callback: () => void): (() => void) => {
+  const shouldAddListener = !windowSizeSubscribers.size && isClient;
+  windowSizeSubscribers.add(callback);
+  if (shouldAddListener) {
+    window.addEventListener('resize', dispatchWindowSizeChange);
+  }
+  return function unsubscribeFromWindowSizeChange() {
+    windowSizeSubscribers.delete(callback);
+    if (!windowSizeSubscribers.size && isClient) {
+      window.removeEventListener('resize', dispatchWindowSizeChange);
     }
-    events.add(handler);
-    const subscription = {
-      unsubscribe: () => {
-        events.delete(handler);
-        if (events.size === 0) {
-          window.removeEventListener('resize', onResize);
-        }
-      },
-    };
-    return subscription;
-  },
+  };
 };
 
-export const useWindowSize = (
-  options: {throttleMs?: number} = {},
-  initialWidth: number = 0,
-  initialHeight: number = 0,
-): State => {
-  const {throttleMs = 100} = options;
+function useWindowSizeOptions({
+  throttleMs = 0,
+  initialWidth = 0,
+  initialHeight = 0,
+}: WindowSizeOptions = {}): Required<WindowSizeOptions> {
+  return useMemo(() => {
+    return {throttleMs, initialWidth, initialHeight};
+  }, [throttleMs, initialWidth, initialHeight]);
+}
 
-  const [size, setSize] = useState({
-    width: isClient ? window.innerWidth : initialWidth,
-    height: isClient ? window.innerHeight : initialHeight,
-  });
+/**
+ * `useWindowSize` is a React hook for components that care about
+ * the window size. It can be used statefully or not.
+ *
+ * @see https://hz-core.netlify.com/use-windowsize
+ */
+function useWindowSize(
+  /**
+   * `handler` will receive a `WindowSize` object each time
+   * the window size changes.
+   */
+  handler: (size: WindowSize) => void,
+  /**
+   * `options` for window size handling.
+   */
+  options?: WindowSizeOptions,
+): void;
+/**
+ * `useWindowSize` is a React hook for components that care about
+ * the window size. It can be used statefully or not.
+ *
+ * @see https://hz-core.netlify.com/use-windowsize
+ */
+function useWindowSize(
+  /**
+   * `options` for window size handling.
+   */
+  options?: WindowSizeOptions,
+): WindowSize;
+function useWindowSize(
+  handlerOrOptions?: ((size: WindowSize) => void) | WindowSizeOptions,
+  windowSizeOptions?: WindowSizeOptions,
+): WindowSize | void {
+  const changeHandler = useRef<((size: WindowSize) => void) | null>(null);
+  if (typeof handlerOrOptions === 'function') {
+    changeHandler.current = handlerOrOptions;
+  } else {
+    windowSizeOptions = handlerOrOptions;
+  }
 
-  const handle = throttle(() => {
-    setSize({
-      width: window.innerWidth,
-      height: window.innerHeight,
-    });
-  }, throttleMs);
+  const options = useWindowSizeOptions(windowSizeOptions);
+  const [size, setSize] = useState<WindowSize>(() => getWindowSize(options));
 
-  useEffect(() => {
-    const subscription = subscriber.subscribe(handle);
-    return () => {
-      subscription.unsubscribe();
-    };
-    /* eslint-disable-next-line react-hooks/exhaustive-deps */
-  }, []);
+  const handleSizeChange = useMemo(() => {
+    /**
+     * `handleSizeChange` will update the current change handler
+     * with a new `WindowSize` whenever the window's size changes.
+     */
+    function handleSizeChange(): void {
+      const cb = changeHandler.current;
+      const size = getWindowSize(options);
+      if (typeof cb === 'function') {
+        cb(size);
+      } else {
+        setSize(size);
+      }
+    }
+    return options.throttleMs
+      ? throttle(handleSizeChange, options.throttleMs)
+      : handleSizeChange;
+  }, [options]);
 
-  return size;
-};
+  useEffect(() => subscribeToWindowSizeChange(handleSizeChange), [
+    handleSizeChange,
+  ]);
+
+  if (!changeHandler.current) {
+    return size;
+  }
+}
+
+export {useWindowSize};
 
 class WindowsizeMonitor extends PureComponent<Props, State> {
   state = {
@@ -69,13 +163,13 @@ class WindowsizeMonitor extends PureComponent<Props, State> {
     // undefined window
     if (typeof window !== undefined) {
       this.updateDimensions();
-      this.subscription = subscriber.subscribe(this.updateDimensions);
+      this.subscription = subscribeToWindowSizeChange(this.updateDimensions);
     }
   }
 
   componentWillUnmount(): void {
     if (this.subscription) {
-      this.subscription.unsubscribe();
+      this.subscription();
       this.subscription = null;
     }
   }
@@ -86,7 +180,7 @@ class WindowsizeMonitor extends PureComponent<Props, State> {
     return {width, height};
   }
 
-  subscription: null | {unsubscribe: () => void} = null;
+  subscription: null | (() => void) = null;
 
   updateDimensions = () => {
     this.setState(this.getDimensions());
