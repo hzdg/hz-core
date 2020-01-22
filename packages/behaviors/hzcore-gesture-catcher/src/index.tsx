f57ccb3f0b98c672c7168028e42538eb64f88ceb
Eric Eldredge <lettertwo@gmail.com>
2020-01-22 12:21:08 -0500

    🎨 (@hzcore/gesture-catcher) export *GestureHandler, Orientation types

--- packages/behaviors/hzcore-gesture-catcher/src/index.tsx
+++ packages/behaviors/hzcore-gesture-catcher/src/index.tsx
@@ -1,4 +1,5 @@
 import * as GestureCatcher from './GestureCatcher';
+import * as GestureObservable from '@hzcore/gesture-observable';
 import * as keyboard from './useKeyboardGesture';
 import * as mouse from './useMouseGesture';
 import * as touch from './useTouchGesture';
@@ -15,28 +16,35 @@ export type GestureCatcherProps<
 export type KeyboardGestureConfig = keyboard.KeyboardGestureConfig;
 export type KeyboardGestureState = keyboard.KeyboardGestureState;
 export type KeyboardGestureEndState = keyboard.KeyboardGestureEndState;
+export type KeyboardGestureHandler = keyboard.KeyboardGestureHandler;
 
 export type MouseGestureConfig = mouse.MouseGestureConfig;
 export type MouseGestureState = mouse.MouseGestureState;
 export type MouseGestureEndState = mouse.MouseGestureEndState;
+export type MouseGestureHandler = mouse.MouseGestureHandler;
 
 export type TouchGestureConfig = touch.TouchGestureConfig;
 export type TouchGestureState = touch.TouchGestureState;
 export type TouchGestureEndState = touch.TouchGestureEndState;
+export type TouchGestureHandler = touch.TouchGestureHandler;
 
 export type WheelGestureConfig = wheel.WheelGestureConfig;
 export type WheelGestureState = wheel.WheelGestureState;
 export type WheelGestureEndState = wheel.WheelGestureEndState;
+export type WheelGestureHandler = wheel.WheelGestureHandler;
 
 export type GestureConfig = gesture.GestureConfig;
 export type GestureState = gesture.GestureState;
 export type GestureEndState = gesture.GestureEndState;
+export type GestureHandler = gesture.GestureHandler;
 
 export const useKeyboardGesture = keyboard.default;
 export const useMouseGesture = mouse.default;
 export const useTouchGesture = touch.default;
 export const useWheelGesture = wheel.default;
 export const useGesture = gesture.default;
+
+export const Orientation = GestureObservable.Orientation;
 
 export const useConfigFor = GestureCatcher.useConfigFor;
 export default GestureCatcher.default;
