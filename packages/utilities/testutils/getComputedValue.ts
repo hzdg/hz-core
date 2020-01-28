bfc6031cb5d9d2adf24b614b63f4358fcf167045
Eric Eldredge <lettertwo@gmail.com>
2020-01-27 19:33:39 -0500

    🚨 (testutils) fix getComputedValue overload types

--- packages/utilities/testutils/getComputedValue.ts
+++ packages/utilities/testutils/getComputedValue.ts
@@ -1,39 +1,34 @@
 /**
- * A getter for a computed style value.
- * The element should be passed to the getter as context,
- * i.e., `computedValueGetter.call(someElement)`.
- */
-export interface ComputedValueGetter<
-  K extends keyof CSSStyleDeclaration,
-  T extends typeof Number | typeof Boolean
-> {
-  (this: Element): T extends typeof Number
-    ? number
-    : T extends typeof Boolean
-    ? boolean
-    : CSSStyleDeclaration[K];
-}
-
-/**
  * `getComputedValue` will return a getter for a value
  * from an Element's computed styles.
  */
-export default function getComputedValue<
+function getComputedValue<K extends keyof CSSStyleDeclaration>(
+  /** The name of the computed style to get, i.e., 'width'. */
+  value: K,
+): (this: Element) => CSSStyleDeclaration[K];
+/**
+ * `getComputedValue` will return a getter for a value
+ * from an Element's computed styles cast as `Number`.
+ */
+function getComputedValue<K extends keyof CSSStyleDeclaration>(
+  /** The name of the computed style to get, i.e., 'width'. */
+  value: K,
+  as: typeof Number,
+): (this: Element) => number;
+/**
+ * `getComputedValue` will return a getter for a value
+ * from an Element's computed styles cast as `Boolean`.
+ */
+function getComputedValue<K extends keyof CSSStyleDeclaration>(
+  /** The name of the computed style to get, i.e., 'width'. */
+  value: K,
+  as: typeof Boolean,
+): (this: Element) => boolean;
+function getComputedValue<
   K extends keyof CSSStyleDeclaration,
   T extends typeof Number | typeof Boolean
->(
-  /**
-   * The name of the computed style to get, i.e., 'width'.
-   */
-  value: K,
-  /**
-   * An optional primitive type to convert the value to.
-   * Can be either `Number` or `Boolean`. Default behavior
-   * is to return the computed value directly (probably `string`).
-   */
-  as?: T,
-): ComputedValueGetter<K, T> {
-  return function(this: Element): CSSStyleDeclaration[K] {
+>(value: K, as?: T) {
+  return function(this: Element) {
     const computed = getComputedStyle(this)[value];
     switch (as) {
       case Number:
@@ -45,3 +40,5 @@ export default function getComputedValue<
     }
   };
 }
+
+export default getComputedValue;
