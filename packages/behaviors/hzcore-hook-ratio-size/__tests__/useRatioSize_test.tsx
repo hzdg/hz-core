aa3f1d600cdd0803c04cb665ad8beee426bc457f
elainen <elaineh@hzdg.com>
2020-02-07 16:54:44 -0500

    ✅ (@hzcore/hook-ratio-size) add more tests for useRatioSize
    Closes #38

--- packages/behaviors/hzcore-hook-ratio-size/__tests__/useRatioSize_test.tsx
+++ packages/behaviors/hzcore-hook-ratio-size/__tests__/useRatioSize_test.tsx
@@ -59,3 +59,92 @@ test('useRatioSize returns the right ratio based on default 6:19 width height be
   const width = parseInt(getByTestId('container-width').textContent || '0');
   expect(Math.floor((height / width) * 1000)).toEqual(3167);
 });
+
+test('useRatioSize uses applicable configurated breakpoint', () => {
+  const RatioSizeUser = (): JSX.Element => {
+    const [playerSize] = useRatioSize({
+      options: {
+        responsiveBreaks: [
+          {
+            breakpoint: 0,
+            ratioSize: {
+              width: 100,
+              height: 100,
+            },
+          },
+        ],
+      },
+    });
+    return (
+      <div>
+        <div data-testid="container-width">{playerSize.width}</div>
+        <div data-testid="container-height">{playerSize.height}</div>
+      </div>
+    );
+  };
+  const {getByTestId} = render(<RatioSizeUser />);
+  const height = parseInt(getByTestId('container-height').textContent || '0');
+  const width = parseInt(getByTestId('container-width').textContent || '0');
+  expect(height).toEqual(100);
+  expect(width).toEqual(100);
+});
+
+// Renderer sets container to 1024x768
+test('useRatioSize does not use non-applicable configurated breakpoint', () => {
+  const RatioSizeUser = (): JSX.Element => {
+    const [playerSize] = useRatioSize({
+      options: {
+        responsiveBreaks: [
+          {
+            breakpoint: 1025,
+            ratioSize: {
+              width: 100,
+              height: 100,
+            },
+          },
+        ],
+      },
+    });
+    return (
+      <div>
+        <div data-testid="container-width">{playerSize.width}</div>
+        <div data-testid="container-height">{playerSize.height}</div>
+      </div>
+    );
+  };
+  const {getByTestId} = render(<RatioSizeUser />);
+  const height = parseInt(getByTestId('container-height').textContent || '0');
+  const width = parseInt(getByTestId('container-width').textContent || '0');
+  expect(height).not.toEqual(100);
+  expect(width).not.toEqual(100);
+});
+
+// Renderer sets container to 1024x768
+test('useRatioSize uses exact configurated breakpoint', () => {
+  const RatioSizeUser = (): JSX.Element => {
+    const [playerSize] = useRatioSize({
+      options: {
+        responsiveBreaks: [
+          {
+            breakpoint: 1024,
+            ratioSize: {
+              width: 100,
+              height: 100,
+            },
+          },
+        ],
+      },
+    });
+    return (
+      <div>
+        <div data-testid="container-width">{playerSize.width}</div>
+        <div data-testid="container-height">{playerSize.height}</div>
+      </div>
+    );
+  };
+  const {getByTestId} = render(<RatioSizeUser />);
+  const height = parseInt(getByTestId('container-height').textContent || '0');
+  const width = parseInt(getByTestId('container-width').textContent || '0');
+  expect(height).toEqual(100);
+  expect(width).toEqual(100);
+});
