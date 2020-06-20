00379aa29c415b338f50aa7ec933148aab81c14d
Eric Eldredge <lettertwo@gmail.com>
2020-06-19 21:13:19 -0400

    ✨ (@hzdg/dom-utils) add isDOMInstance util

--- packages/utilities/dom-utils/src/index.tsx
+++ packages/utilities/dom-utils/src/index.tsx
@@ -1,6 +1,7 @@
 import ensureDOMInstance from './ensureDOMInstance';
 import getDocument from './getDocument';
 import getWindow from './getWindow';
+import isDOMInstance from './isDOMInstance';
 
-export {ensureDOMInstance, getDocument, getWindow};
-export default {ensureDOMInstance, getDocument, getWindow};
+export {ensureDOMInstance, getDocument, getWindow, isDOMInstance};
+export default {ensureDOMInstance, getDocument, getWindow, isDOMInstance};
