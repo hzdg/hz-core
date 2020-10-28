73dfd607bd9c49abf310742948ce94e800110aa7
Eric Eldredge <lettertwo@gmail.com>
2020-10-28 12:55:32 -0400

    ♻️ (@hzdg/sticky) extract getContainingElement to dom-utils

--- packages/ui-components/sticky/src/useContainingElementRef.tsx
+++ packages/ui-components/sticky/src/useContainingElementRef.tsx
@@ -1,55 +1,5 @@
 import {useRef, useMemo} from 'react';
-
-type NodeLike = Node | {node: Node} | {element: Node};
-
-const isClient =
-  typeof window !== 'undefined' &&
-  typeof Document !== 'undefined' &&
-  typeof HTMLElement !== 'undefined';
-
-function getNode(node: NodeLike | null): NodeLike | null {
-  if (node) {
-    node = 'node' in node ? node.node : node;
-    node = 'element' in node ? node.element : node;
-  }
-  return node;
-}
-
-function findAncestor(
-  node: HTMLElement | null,
-  predicate: (node: HTMLElement) => boolean,
-): HTMLElement | null {
-  while (node) {
-    if (predicate(node)) {
-      return node;
-    }
-    node = node.parentElement;
-  }
-  return null;
-}
-
-function getContainingElement(node: HTMLElement | null): HTMLElement | null {
-  if (!isClient) return null;
-  node = getNode(node) as HTMLElement;
-  if (!node) return null;
-  /**
-   * The containing element may be the nearest ancestor element
-   * that has any of the following:
-   *
-   * 1. A position value other than static
-   *    (fixed, absolute, relative, or sticky)
-   * 2. A transform value other than none
-   * 3. A will-change value of transform
-   */
-  const ancestor = findAncestor(node.parentElement, node => {
-    const {position, transform, willChange} = getComputedStyle(node);
-    if (position && position !== 'static') return true;
-    if (transform && transform !== 'none') return true;
-    if (/transform/.test(willChange)) return true;
-    return false;
-  });
-  return ancestor || document.documentElement;
-}
+import {getContainingElement} from '@hzdg/dom-utils';
 
 function isDescendantOf(
   maybeAncestor: HTMLElement | null,
@@ -57,12 +7,7 @@ function isDescendantOf(
 ): boolean {
   if (!maybeAncestor || !node) return false;
   if (maybeAncestor === node) return false;
-  let parent = node.parentElement;
-  while (parent) {
-    if (parent === maybeAncestor) return true;
-    parent = parent.parentElement;
-  }
-  return false;
+  return maybeAncestor.contains(node);
 }
 
 /**
