b1a0677f1339320c5d035cc8ce19aa9ea67e6002
Eric Eldredge <lettertwo@gmail.com>
2020-10-28 12:34:16 -0400

    ✨ (@hzdg/dom-utils) add getNearestScrollNode util

--- packages/utilities/dom-utils/src/index.tsx
+++ packages/utilities/dom-utils/src/index.tsx
@@ -1,7 +1,27 @@
 import ensureDOMInstance from './ensureDOMInstance';
+import findAncestor from './findAncestor';
 import getDocument from './getDocument';
+import getNearestScrollNode from './getNearestScrollNode';
 import getWindow from './getWindow';
+import isClient from './isClient';
 import isDOMInstance from './isDOMInstance';
 
-export {ensureDOMInstance, getDocument, getWindow, isDOMInstance};
-export default {ensureDOMInstance, getDocument, getWindow, isDOMInstance};
+export {
+  ensureDOMInstance,
+  findAncestor,
+  getDocument,
+  getNearestScrollNode,
+  getWindow,
+  isClient,
+  isDOMInstance,
+};
+
+export default {
+  ensureDOMInstance,
+  findAncestor,
+  getDocument,
+  getNearestScrollNode,
+  getWindow,
+  isClient,
+  isDOMInstance,
+};
